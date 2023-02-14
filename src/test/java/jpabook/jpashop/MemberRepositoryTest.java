e285d973af3b40f7a37b877acdeb80f75d6dfb98
ParkMinJun <thankq01@naver.com>
2023-02-14 18:13:14 +0900

    jpa + spring

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -30,7 +30,7 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 
-        //sdfasdf
+        //데이터베이스 파일을 키고 해야 하는데 어떻게 하는지 모르겠어
 
     }
 }
