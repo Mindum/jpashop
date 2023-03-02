853d344cf79e0f57fcc2359022089d2f5e1785d9
ParkMinJun <thankq01@naver.com>
2023-03-02 10:57:37 +0900

    jpa + spring

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -16,9 +16,6 @@ d
         em.persist(member);
         return member.getId();
     }
-    public Long save1(Member member){
-        em.persist(member);
-    }
 
     public Member find(Long id){
         return em.find(Member.class, id);
