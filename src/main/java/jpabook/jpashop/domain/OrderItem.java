9ea9ecd2d28e491b758d93ac25bbbd037a1e21d1
ParkMinJun <thankq01@naver.com>
2023-03-09 16:49:09 +0900

    jpa + spring

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,18 +1,26 @@
-package jpabook.jpashop;
+package jpabook.jpashop.domain;
 
 
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
-public class Member {
+public class OrderItem {
 
     @Id @GeneratedValue
-    private Long id;
-    private String username;
+    @Column(name = "order_item_id")
+    private  Long id;
+
+    private  Item item;
+
+    private  Order order
+
+
+
 }
